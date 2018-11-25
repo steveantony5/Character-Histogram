12895a11e31cde9b577a5c3f053a994fdae637e4
Steve Antony <43216285+steveantony5@users.noreply.github.com>
2018-11-25 13:34:46 -0700

    Update main.c

--- main.c
+++ main.c
@@ -13,17 +13,17 @@
 //
 //****************************************************************************/
 
-//***********************************************************************************
-// Include files
-//***********************************************************************************
+
 #include "main.h"
-#include "common_variable.h"
-//***********************************************************************************
-// Function definition
-//***********************************************************************************
-int main()
+
+#ifdef FRDM
+#include "MKL25Z4.h"
+#endif
+
+
+int main(void)
 {
-	CB RX_Buffer;
+#ifdef LINUX
 	init_CB(&RX_Buffer,5);
 	insert_data(&RX_Buffer,3);
 	insert_data(&RX_Buffer,10);
@@ -40,5 +40,44 @@ int main()
 	insert_data(&TX_Buffer,9);
 	insert_data(&TX_Buffer,2);
 	report_data(&TX_Buffer);
+#endif
+
+#ifdef FRDM
+
+	int16_t database[256] = {0};
+	uartinit();
+	RX_interrupt_init();
+	send_to_console_str("Welcome to Character Histogram Application\r\n");
+
+	init_CB(&RX_buffer, SIZE_OF_RX_CB);
+
+	while(1)
+	{
+		while(!(IsEMPTY(&RX_buffer)))
+		{
+			uint8_t data = 0;
+			char num[4];
+
+			pop_data(&RX_buffer,&data);
+
+			database[data] = database[data] + 1;
+
+			if(data == 13)
+			{
+				PRINT("\r\n\r\n");
+				PRINT("Report:\r\n");
+				for(int i =0;i<256;i++)
+				{
+					if(database[i]!= 0)
+					{
+						sprintf(num," %c - %d \r\n",i,database[i]);
+						PRINT(num);
+					}
+				}
+			}
+		}
+
+	}
+#endif
 
 }
