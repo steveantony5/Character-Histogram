57b831d851cff557d8f52bff1d119741e870a23e
Steve Antony <43216285+steveantony5@users.noreply.github.com>
2018-11-26 15:15:26 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,83 +1,80 @@
-//*****************************************************************************
-// Name        : main
-//
-// Description : Function to call other functions
-//
-// Author      : Steve and Swarupa
-//
-// Date        : Nov 06, 2018
-//
-// Arguments   : No
-//
-// return      : unused
-//
-//****************************************************************************/
 
 
 #include "main.h"
 
 #ifdef FRDM
 #include "MKL25Z4.h"
+
+#define SYSTICK_CTRL (*((volatile unsigned long *) (0xE000E010)))
+#define SYSTICK_LOAD (*((volatile unsigned long *) (0xE000E014)))
+#define SYSTICK_VAL (*((volatile unsigned long *) (0xE000E018)))
 #endif
 
-
+uint32_t database[256] = {0};
+uint8_t data_pop = 0;
+int8_t flag_report = 0;
+char num[20];
+int8_t RX_received = 0;
+int count = 0;
 int main(void)
 {
 #ifdef LINUX
-	init_CB(&RX_buffer,1);
-	insert_data(&RX_buffer,3);
-	report_data(&RX_buffer);
-
-insert:
-	if(insert_data(&RX_buffer,3)== OVERFLOW )
-	{
-		resize_CB(&RX_buffer,((RX_buffer.max_size)*2));
-		goto insert;
-	}
-
-	report_data(&RX_buffer);
+	init_CB(&RX_Buffer,5);
+	insert_data(&RX_Buffer,3);
+	insert_data(&RX_Buffer,10);
 
 
 
+	report_data(&RX_Buffer);
 
+	delete_data(&RX_Buffer);
+	report_data(&RX_Buffer);
+
+	CB TX_Buffer;
+	init_CB(&TX_Buffer,1);
+	insert_data(&TX_Buffer,9);
+	insert_data(&TX_Buffer,2);
+	report_data(&TX_Buffer);
 #endif
 
 #ifdef FRDM
 
-	int16_t database[256] = {0};
+
 	uartinit();
 	RX_interrupt_init();
+
 	send_to_console_str("Welcome to Character Histogram Application\r\n");
 
 	init_CB(&RX_buffer, SIZE_OF_RX_CB);
 
 	while(1)
 	{
-		while(!(IsEMPTY(&RX_buffer)))
-		{
-			uint8_t data = 0;
-			char num[4];
-
-			pop_data(&RX_buffer,&data);
-
-			database[data] = database[data] + 1;
-
-			if(data == 13)
-			{
-				PRINT("\r\n\r\n");
-				PRINT("Report:\r\n");
-				for(int i =0;i<256;i++)
-				{
-					if(database[i]!= 0)
-					{
-						sprintf(num," %c - %d \r\n",i,database[i]);
-						PRINT(num);
-					}
-				}
-			}
-		}
 
 	}
+
 #endif
 
+}
+
+
+void sys_reload()
+{
+	SYSTICK_VAL = 0x0;    //clear current timer value
+	SYSTICK_LOAD = 0xFFFFFFFF;
+	SYSTICK_CTRL = 0x7;
+}
+
+void SysTick_Handler(void)
+{
+	PRINT("\r\n\r\n");
+	PRINT("Report:\r\n");
+	for(int i =0;i<256;i++)
+	{
+		if(database[i]!= 0)
+		{
+			sprintf(num," %c - %d \r\n",i,database[i]);
+			PRINT(num);
+		}
+	}
+	SYSTICK_CTRL = 0x0;
 }
